bff6933b5194773e566f4e89325ba6d22442736e
FlyingEwok <71281801+FlyingEwok@users.noreply.github.com>
2020-11-08 20:46:48 -0500

    changes to main menu

--- mainMenu.py
+++ mainMenu.py
@@ -6,12 +6,15 @@ import main
 # pylint: disable=no-member
 
 pygame.init()
+pygame.display.set_caption("Escape The Space Station!")
+icon = pygame.image.load('images/astronaut.png')
+pygame.display.set_icon(icon)
 surface = pygame.display.set_mode((1280, 720))
 
 def start_the_game():
     main.main()
 
-menu = pygame_menu.Menu(720, 1280, 'Welcome', theme=pygame_menu.themes.THEME_SOLARIZED)
+menu = pygame_menu.Menu(720, 1280, 'Escape The Space Station!', theme=pygame_menu.themes.THEME_SOLARIZED)
 
 menu.add_button('Play', start_the_game)
 menu.add_button('Quit', pygame_menu.events.EXIT)
