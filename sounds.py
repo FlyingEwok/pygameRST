4ab9c789ea975839596c72fed691255c5870f0c1
FlyingEwok <71281801+FlyingEwok@users.noreply.github.com>
2020-11-10 17:21:40 -0500

    removed commented out stuff

--- sounds.py
+++ sounds.py
@@ -9,7 +9,3 @@ playerDeathSound = pygame.mixer.Sound('sounds/PlayerDeathSound.wav')
 #levelCompletedSound = pygame.mixer.Sound('sounds/LevelCompletedSound.wav')
 jumpSound = pygame.mixer.Sound('sounds/JumpSound.wav')
 collectKeySound = pygame.mixer.Sound('sounds/CollectKeySound.wav')
-
-# Background Music
-# levelTheme = pygame.mixer.music.load('sounds/LevelTheme.wav')
-# menuTheme = pygame.mixer.music.load('sounds/MenuTheme.wav')
